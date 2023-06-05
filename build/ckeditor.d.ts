c3a60031f634a8f8fcfe4194d046b6906bfb42f8
billy <braimbau@student.42.fr>
2023-06-05 14:12:36 +0200

    test

--- build/ckeditor.d.ts
+++ build/ckeditor.d.ts
@@ -9,7 +9,7 @@ import { Link } from '@ckeditor/ckeditor5-link';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Table } from '@ckeditor/ckeditor5-table';
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
 import { List } from '@ckeditor/ckeditor5-list';
 import { Highlight } from '@ckeditor/ckeditor5-highlight';
 import Image from '@ckeditor/ckeditor5-image/src/image';
@@ -18,14 +18,5 @@ import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageUpload';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 export default class ClassicEditor extends ClassicEditorBase {
-    static builtinPlugins: (typeof Essentials | typeof Bold | typeof Italic | typeof Underline | typeof SimpleUploadAdapter | typeof Image | typeof ImageResize | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof Link | typeof Paragraph | typeof Heading | typeof Table | typeof List | typeof Highlight)[];
-    static defaultConfig: {
-        toolbar: {
-            items: string[];
-        };
-        table: {
-            contentToolbar: string[];
-        };
-        language: string;
-    };
+    static builtinPlugins: (typeof Essentials | typeof Bold | typeof Italic | typeof Underline | typeof SimpleUploadAdapter | typeof Image | typeof ImageResize | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof Link | typeof Paragraph | typeof Heading | typeof Table | typeof TableToolbar | typeof List | typeof Highlight)[];
 }
